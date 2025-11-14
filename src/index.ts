0bdfff4a3080789c9f1d5f5adefd2091ea5270a9
Isai Medina <isai@zettabrasil.com.br>
2025-11-14 19:33:14 -0300

    chore: actualizado a la versión 0.0.2

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as loader from './loader.js';
 const cliName = 'Patto CLI';
 const program = new Command()
     .name(cliName)
-    .version(`${cliName} v0.0.1`, '-v, --version')
+    .version(`${cliName} v0.0.2`, '-v, --version')
     .description('Herramienta de línea de comandos para trabajar con Patto Bot Template');
 
 Object.values(loader).forEach((command) => {
